0a38fad00675368304f06884eced3880fe0c8446
Ebba Bernbro <ebbabernbro@gmail.com>
2022-11-12 17:41:51 +0100

    Add a new function to handle the location request and changes made to the existing function to fetch weather data

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
-// C O D E   F O R   D I S P L A Y I N G   C O N T E N T   O N   T H E   P A G E   F R O M   R E Q U E S T   F U N C T I O N
+let apiKey = "466ef4685d34153b24ce25e39f641f6a";
+
+// C O D E   F O R   D O I N G   A   R E Q U E S T
+//Fetch coordinates for a location
+function fetchLocation() {
+  let city = "abisko";
+  let url =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&limit=1&appid=" +
+    apiKey;
+
+  fetch(url)
+    //The then() method returns a Promise.
+    .then((response) => response.json()) //Returns data as a JSON Object
+    .then((data) => fetchWeather(data));
+}
+
+//Calling the function
+fetchLocation();
+
+// C O D E   F O R   D O I N G   A   R E Q U E S T
+//Fetch weather for a location
+function fetchWeather(coords) {
+  const latitude = coords[0].lat;
+  const longitude = coords[0].lon;
+
+  /*unit parameter in API call as Kelvin is used by default.
+    Metric could be changed to imperial for Fahrenheit*/
+  let celsius = "&units=metric";
+  let url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    celsius +
+    "&appid=" +
+    apiKey;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => renderWeather(data));
+}
+
+/*
+C O D E   F O R   D I S P L A Y I N G   C O N T E N T   O N   
+T H E   P A G E   F R O M   R E Q U E S T   F U N C T I O N 
+*/
 function renderWeather(forecast) {
   //Referens to the HTML container with the ID weatherData
   let forecastDetails = document.getElementById("weatherData");
@@ -17,18 +64,3 @@ function renderWeather(forecast) {
   <div class="description">Description: ${localWeatherDescription}</div>
   `;
 }
-
-// C O D E   F O R   D O I N G   T H E   R E Q U E S T
-//Fetch weather data for a location - function
-function fetchWeather() {
-  let url =
-    "https://api.openweathermap.org/data/2.5/weather?lat=68.3501695&lon=18.8300059&units=metric&appid=466ef4685d34153b24ce25e39f641f6a";
-
-  //Fetch API guide at:https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => renderWeather(data));
-}
-
-//Calling the function
-fetchWeather();
