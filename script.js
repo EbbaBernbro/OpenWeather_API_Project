f3ccc5e95c44617c32bf3a8446600b54860bedd4
Ebba Bernbro <ebbabernbro@gmail.com>
2022-11-15 17:35:07 +0100

    Add comments and change all let to const

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-let apiKey = "466ef4685d34153b24ce25e39f641f6a";
+const apiKey = "466ef4685d34153b24ce25e39f641f6a";
 
 // C O D E   F O R   D O I N G   A   R E Q U E S T
 //Fetch coordinates for a location
 async function fetchLocation() {
-  let city = "abisko";
-  let url =
+  const city = "abisko";
+  const url =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
     "&limit=1&appid=" +
     apiKey;
 
   await fetch(url) //We call fetch
-    //The then() method returns a Promise.
+    /* We handle the sucess case with .then, we get the response from below which returns data that we need to convert to JSON.
+    response.json returns a promise. We can chain on a .then to handle that promise. Then we get the actual data and
+    we use that (parameter) in our featchWeather. */
+
     .then((response) => response.json())
-    /*
-    We handle the sucess case with .then,
-    we get the response from below which returns data that 
-    we need to convert to JSON.
-    response.json returns a promise. We can chain on a .then
-    to handle that promise. Then we get the actual data and
-    we use that (parameter) in our featchWeather.
-    */
     .then((data) => fetchWeather(data));
 }
 
@@ -30,13 +25,14 @@ fetchLocation();
 // C O D E   F O R   D O I N G   A   R E Q U E S T
 //Fetch weather for a location
 async function fetchWeather(coordinates) {
+  //coordinates[0].lat/lon = get latitude and longitude from object.
   const latitude = coordinates[0].lat;
   const longitude = coordinates[0].lon;
 
   /*unit parameter in API call as Kelvin is used by default.
     Metric could be changed to imperial for Fahrenheit*/
-  let celsius = "&units=metric";
-  let url =
+  const celsius = "&units=metric";
+  const url =
     "https://api.openweathermap.org/data/2.5/weather?lat=" +
     latitude +
     "&lon=" +
@@ -56,15 +52,15 @@ T H E   P A G E   F R O M   R E Q U E S T   F U N C T I O N
 */
 function renderWeather(forecast) {
   //Referens to the HTML container with the ID weatherData
-  let forecastDetails = document.getElementById("weatherData");
+  const forecastDetails = document.getElementById("weatherData");
 
   //variables for storing data
-  let location = forecast.name;
-  let localTemp = Math.round(forecast.main.temp);
+  const location = forecast.name;
+  const localTemp = Math.round(forecast.main.temp);
 
   /*What I learned from the variable below is that ${forecast.weather[0].description} 
     will give me undefined wheras storing it in this variable works.*/
-  let localWeatherDescription = forecast.weather[0].description;
+  const localWeatherDescription = forecast.weather[0].description;
 
   forecastDetails.innerHTML = `
   <h2 class="location">${location}</h2>
